212ff162494deba804d3f984efbaf5ea1def17a5
Wim Joris <wim@squeezely.tech>
2018-10-08 16:36:57 +0200

    Prepare for unit testing

---
+++ src/Tests/Pooper/QueryDumperTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Jspeedz\DebugPooper\Tests\Pooper;
+
+use Jspeedz\DebugPooper\Pooper\QueryDumper;
+use PHPUnit\Framework\TestCase;
+
+class QueryDumperTest extends TestCase {
+    public function testQueryDumper() {
+//        QueryDumper::dump('SELECT 1 FROM x');
+    }
+}
