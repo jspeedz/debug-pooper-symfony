107668d2dc97264bb9a08c639de3f9e51858fde9
Wim Joris <wim@squeezely.tech>
2018-10-08 16:12:14 +0200

    Add dumpquery() and make methods static

---
+++ src/Component/VarDumper/Resources/functions/dumpquery.php
@@ -0,0 +1,16 @@
+<?php
+use Jspeedz\DebugPooper\Pooper\QueryDumper;
+
+if(!function_exists('💩')) {
+    /**
+     * @param string $query
+     * @param array $parameters
+     * @param int[] $types
+     * @return void
+     *
+     * @throws \Jspeedz\Exception\InvalidParameterException
+     */
+    function dumpQuery(string $query, array $parameters = [], array $types = []): void {
+        QueryDumper::dump($query, $parameters, $types);
+    }
+}
