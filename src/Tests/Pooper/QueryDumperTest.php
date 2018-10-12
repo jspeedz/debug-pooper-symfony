554dcc09effb0b3eecec5b5a9597438a4f2bbcae
Wim Joris <wim@squeezely.tech>
2018-10-12 10:01:08 +0200

    Add a few simple unit tests

--- src/Tests/Pooper/QueryDumperTest.php
+++ src/Tests/Pooper/QueryDumperTest.php
@@ -2,10 +2,55 @@
 namespace Jspeedz\DebugPooper\Tests\Pooper;
 
 use Jspeedz\DebugPooper\Pooper\QueryDumper;
+use PDO;
 use PHPUnit\Framework\TestCase;
 
 class QueryDumperTest extends TestCase {
-    public function testQueryDumper() {
-//        QueryDumper::dump('SELECT 1 FROM x');
+    public function testQueryDumperWithoutParameters() {
+        $result = QueryDumper::dump('SELECT 1 FROM x', [], [], true);
+
+        $this->assertEquals('SELECT 1 FROM x', $result);
+    }
+
+    public function testQueryDumperWithParameters() {
+        $result = QueryDumper::dump('SELECT 1 FROM x WHERE x.y = ? OR x.z = ?', [
+            1,
+            '2',
+        ], [], true);
+
+        $this->assertEquals('SELECT 1 FROM x WHERE x.y = 1 OR x.z = 2', $result);
+    }
+
+    public function testQueryDumperWithAliasedParameters() {
+        $result = QueryDumper::dump('SELECT 1 FROM x WHERE x.y = :param1 OR x.z = :param2', [
+            'param1' => 1,
+            'param2' => '2',
+        ], [], true);
+
+        $this->assertEquals('SELECT 1 FROM x WHERE x.y = 1 OR x.z = 2', $result);
+    }
+
+    public function testQueryDumperWithParametersAndTypes() {
+        $result = QueryDumper::dump('SELECT 1 FROM x WHERE x.y = ? OR x.z = ?', [
+            1,
+            2,
+        ], [
+            PDO::PARAM_INT,
+            PDO::PARAM_STR,
+        ], true);
+
+        $this->assertEquals('SELECT 1 FROM x WHERE x.y = 1 OR x.z = "2"', $result);
+    }
+
+    public function testQueryDumperWithAliasedParametersAndTypes() {
+        $result = QueryDumper::dump('SELECT 1 FROM x WHERE x.y = :param1 OR x.z = :param2', [
+            'param1' => 1,
+            'param2' => 2,
+        ], [
+            PDO::PARAM_INT,
+            PDO::PARAM_STR,
+        ], true);
+
+        $this->assertEquals('SELECT 1 FROM x WHERE x.y = 1 OR x.z = "2"', $result);
     }
 }
